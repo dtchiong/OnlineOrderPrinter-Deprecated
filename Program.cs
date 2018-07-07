d39ea9f125522aa227e9b6ccd4c71039de771e9c
Derek <dtchiong@gmail.com>
2018-07-06 21:04:42 -0700

    connects to gmail API and extracts order #

---
+++ Program.cs
@@ -0,0 +1,107 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Gmail.v1;
+using Google.Apis.Gmail.v1.Data;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+
+using HtmlAgilityPack;
+
+using System;
+using System.Xml;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GmailQuickstart {
+    class Program {
+        // If modifying these scopes, delete your previously saved credentials
+        // at ~/.credentials/gmail-dotnet-quickstart.json
+        static string[] Scopes = { GmailService.Scope.GmailReadonly };
+        static string ApplicationName = "Gmail API .NET Quickstart";
+
+        static void Main(string[] args) {
+            UserCredential credential;
+
+            using (var stream =
+                new FileStream("credentials.json", FileMode.Open, FileAccess.Read)) {
+                string credPath = System.Environment.GetFolderPath(
+                    System.Environment.SpecialFolder.Personal);
+                credPath = Path.Combine(credPath, ".credentials/gmail-dotnet-quickstart.json");
+
+                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.Load(stream).Secrets,
+                    Scopes,
+                    "user",
+                    CancellationToken.None,
+                    new FileDataStore(credPath, true)).Result;
+                Console.WriteLine("Credential file saved to: " + credPath);
+            }
+
+            // Create Gmail API service.
+            var service = new GmailService(new BaseClientService.Initializer() {
+                HttpClientInitializer = credential,
+                ApplicationName = ApplicationName,
+            });
+
+            // Define parameters of request.
+            UsersResource.LabelsResource.ListRequest request = service.Users.Labels.List("me");
+            /*
+            // List labels.
+            IList<Label> labels = request.Execute().Labels;
+            Console.WriteLine("Labels:");
+            if (labels != null && labels.Count > 0) {
+                foreach (var labelItem in labels) {
+                    Console.WriteLine("{0}", labelItem.Name);
+                }
+            } else {
+                Console.WriteLine("No labels found.");
+            }
+            */
+            var emailResponse = GetMessage(service, "t4milpitas@gmail.com", "1646cc5338cba4c1");
+            var body = emailResponse.Payload.Body.Data;
+            byte[] data = FromBase64ForUrlString(body);
+            string decodedBody = Encoding.UTF8.GetString(data);
+            //Console.WriteLine(decodedBody);
+            ScanGrubHub(decodedBody);
+
+            Console.Read();
+        }
+
+        public static Message GetMessage(GmailService service, String userId, String messageId) {
+            try {
+                return service.Users.Messages.Get(userId, messageId).Execute();
+            } catch (Exception e) {
+                Console.WriteLine("An error occurred: " + e.Message);
+            }
+
+            return null;
+        }
+
+        //Converting function
+        public static byte[] FromBase64ForUrlString(string base64ForUrlInput) {
+            int padChars = (base64ForUrlInput.Length % 4) == 0 ? 0 : (4 - (base64ForUrlInput.Length % 4));
+            StringBuilder result = new StringBuilder(base64ForUrlInput, base64ForUrlInput.Length + padChars);
+            result.Append(String.Empty.PadRight(padChars, '='));
+            result.Replace('-', '+');
+            result.Replace('_', '/');
+            return Convert.FromBase64String(result.ToString());
+        }
+
+        
+        public static void ScanGrubHub(string html) {
+            var htmlDoc = new HtmlDocument();
+            htmlDoc.LoadHtml(html);
+
+            var orderNumberPath = htmlDoc.DocumentNode.SelectSingleNode("//body/table/tbody/tr/td/table/tbody/tr/td/table[2]/tbody/tr/th/table/tbody/tr/th/div/div[4]/span[2]");
+            //var pickupByNamePath = htmlDoc.DocumentNode.SelectSingleNode("//body/table/tbody/tr/td/table/tbody/tr/td/table/tbody")
+            Console.WriteLine("Order Number: "+ orderNumberPath.InnerHtml);
+       
+            
+        }
+        
+    }
+
+}
