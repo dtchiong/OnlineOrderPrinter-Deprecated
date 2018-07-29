e988dc75370b0980ba2b5185f379c9c28ae127d5
Derek <dtchiong@gmail.com>
2018-07-29 02:00:23 -0700

    Refactored OrderToArray() into 2 methods, fixed off by 1 char substring, fixed Exception by opening connection to printer before setting language to ZPL

--- PrinterUtility.cs
+++ PrinterUtility.cs
@@ -23,7 +23,9 @@ namespace GmailQuickstart {
             Console.WriteLine("In Constructor");
             try {
                 printerConn = FindConnection();
+                printerConn.Open();
                 SetPrintLangauge(printerConn);
+                printerConn.Close();
                 Console.WriteLine("Construtor methods worked");
             } catch(Exception e) {
                 Console.WriteLine(e.ToString());
@@ -32,6 +34,7 @@ namespace GmailQuickstart {
 
         /* Returns the connection to the zebra printer after finding it through the Usb Discoverer */
         public Connection FindConnection() {
+            Console.WriteLine("START: FindConnection()");
             //Finds all the USB connected Zebra printer drivers
             List<DiscoveredPrinterDriver> discoveredPrinterDrivers = UsbDiscoverer.GetZebraDriverPrinters();
             if (discoveredPrinterDrivers == null) {
@@ -43,6 +46,7 @@ namespace GmailQuickstart {
             DiscoveredPrinterDriver printerDriver = discoveredPrinterDrivers[0];
             Console.WriteLine(printerDriver);
 
+            Console.WriteLine("END: FindConnection()");
             //Get the connection to the printer driver
             return printerDriver.GetConnection();
         }
@@ -78,7 +82,7 @@ namespace GmailQuickstart {
          *   12 |   48    |"Special Instructions2" 
          */
         public void PrintOrder(string[][] items) {
-            Console.WriteLine("In PrintOrder()");
+            Console.WriteLine("START: PrintOrder()");
             try {
                 
                 if (printerConn == null) {
@@ -88,15 +92,17 @@ namespace GmailQuickstart {
 
                 //We open the connection to printer, then get the instance of the printer
                 try {
+                    Console.WriteLine("PrinterOrder() - before opening connection");
                     printerConn.Open();
                     ZebraPrinter printer = ZebraPrinterFactory.GetInstance(PrinterLanguage.ZPL, printerConn);
-
-                    Console.WriteLine("Printer Connection opened and got instance");
+                    
+                    Console.WriteLine("PrinterOrder() - got printer instance");
                     for (int i=0; i<items.Length; i++) {
                         printer.PrintStoredFormat(PrintTemplatePath, items[i]);
                     }
 
                 }catch(ConnectionException e) {
+                    Console.WriteLine("PrinterOrder()-103- something messed up");
                     Console.WriteLine(e.ToString());
                 }finally {
                     printerConn.Close();
@@ -113,7 +119,6 @@ namespace GmailQuickstart {
          * array that represents the fields of the label that will be printed out.
          * The length of this 2-dim array is the number of items in the given order.
          * Used for supplying fields to fill PrintStoredFormat's template
-         * -- Need to refactor into 2 methods
          */
         private string[][] OrderToArray(Order order) {
 
@@ -124,44 +129,56 @@ namespace GmailQuickstart {
             for (int i=0; i< orderArr.Length; i++) {
 
                 //Initialize array for each item
-                orderArr[i] = new string[itemArrSize];
-                //Fill values within the offeset with empty strings since they aren't going to be used
-                for (int j = 0; j < ArrayFieldOffset; j++) {
-                    orderArr[i][j] = "";
-                }
+                orderArr[i] = new string[itemArrSize]; //the array representing 1 item
+                Item item   = itemsList[i];            //the item used to fill the array
 
-                Item item = itemsList[i];
-                //Construct actual values for the indices corresponding to fields
-                orderArr[i][ArrayFieldOffset    ] = item.ItemCount;
-                orderArr[i][ArrayFieldOffset + 1] = order.Service;
-                orderArr[i][ArrayFieldOffset + 2] = order.CustomerName;
-                orderArr[i][ArrayFieldOffset + 3] = item.ItemName;
-                orderArr[i][ArrayFieldOffset + 4] = (item.Size == "Large") ? "Large" : "";
-                orderArr[i][ArrayFieldOffset + 5] = (item.Temperature == "Hot") ? "Hot" : "";
-                orderArr[i][ArrayFieldOffset + 6] = (item.IceLevel != "Standard") ? item.IceLevel : "";
-                orderArr[i][ArrayFieldOffset + 7] = (item.SugarLevel != "Standard") ? item.SugarLevel : "";
-                orderArr[i][ArrayFieldOffset + 8] = (item.MilkSubsitution != null) ? item.MilkSubsitution : "";
-
-                string addOns = "";
-                if (item.AddOnList != null) {
-                    foreach (string addOn in item.AddOnList) {
-                        addOns = addOns + addOn + ", ";
-                    }
-                }
-                orderArr[i][ArrayFieldOffset + 9] = addOns;
-
-                orderArr[i][ArrayFieldOffset + 10] = "";
-                orderArr[i][ArrayFieldOffset + 11] = "";
-                if (item.SpecialInstructions != null && item.SpecialInstructions.Length > 48) {
-                    orderArr[i][ArrayFieldOffset + 10] = item.SpecialInstructions.Substring(0, 47);
-                    orderArr[i][ArrayFieldOffset + 11] = item.SpecialInstructions.Substring(48);
-                }
+                FillItemArray(orderArr[i], order, item);
             }
             return orderArr;
         }
 
+        /* Fills the array with the fields of the item. The offset is required 
+         * because that's where the values start when read by the PrintStoredFormat()
+         */
+        private void FillItemArray(string[] fields, Order order, Item item) {
+            const int instructionsCharLim = 48;
+            //Fill values within the offeset with empty strings since they aren't going to be used
+            for (int i = 0; i < ArrayFieldOffset; i++) {
+                fields[i] = "";
+            }
+
+            //Construct actual values for the indices corresponding to fields
+            fields[ArrayFieldOffset    ] = item.ItemCount;
+            fields[ArrayFieldOffset + 1] = order.Service;
+            fields[ArrayFieldOffset + 2] = order.CustomerName;
+            fields[ArrayFieldOffset + 3] = item.ItemName;
+            fields[ArrayFieldOffset + 4] = (item.Size == "Large") ? "Large" : "";
+            fields[ArrayFieldOffset + 5] = (item.Temperature == "Hot") ? "Hot" : "";
+            fields[ArrayFieldOffset + 6] = (item.IceLevel != "Standard") ? item.IceLevel : "";
+            fields[ArrayFieldOffset + 7] = (item.SugarLevel != "Standard") ? item.SugarLevel : "";
+            fields[ArrayFieldOffset + 8] = (item.MilkSubsitution != null) ? item.MilkSubsitution : "";
+
+            string addOns = "";
+            if (item.AddOnList != null) {
+                foreach (string addOn in item.AddOnList) {
+                    addOns = addOns + addOn + ", ";
+                }
+            }
+            fields[ArrayFieldOffset + 9] = addOns;
+
+            fields[ArrayFieldOffset + 10] = "";
+            fields[ArrayFieldOffset + 11] = "";
+
+            string instruction = item.SpecialInstructions;
+            if (instruction != null && instruction.Length > instructionsCharLim) {
+                fields[ArrayFieldOffset + 10] = instruction.Substring(0, instructionsCharLim);
+                fields[ArrayFieldOffset + 11] = instruction.Substring(instructionsCharLim);
+            }
+        }
+
         /* Sets the page description langauge to ZPL */
         private bool SetPrintLangauge(Connection conn) {
+            Console.WriteLine("START: Set PrintLanguage()");
             string pageDescriptionLanguage = "zpl";
 
             //Set the print langaugein the printer
@@ -173,14 +190,14 @@ namespace GmailQuickstart {
                 Console.WriteLine("Error: " + pageDescriptionLanguage + " not found- Not a ZPL Printer");
                 return false;
             }
-
+            Console.WriteLine("END: PrintLanguage() - language set");
             return true;
         }
 
         /* Returns true if the printer is ready to print, else false 
          * Called before printing 
          */
-        private bool checkPrinterStatus(Connection conn) {
+        private bool CheckPrinterStatus(Connection conn) {
             ZebraPrinter printer = ZebraPrinterFactory.GetLinkOsPrinter(conn);
             if (printer == null) {
                 printer = ZebraPrinterFactory.GetInstance(PrinterLanguage.ZPL, conn);
